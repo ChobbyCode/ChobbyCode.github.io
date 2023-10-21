6d02d92b4bc729b89adda5b3346e002a1f65c83e
ChobbyCode <jacobhadfield99@gmail.com>
2023-10-21 11:53:53 +0100

    Added Mobile Warning To Home Page

--- assets/scripts/checkDeviceType.js
+++ assets/scripts/checkDeviceType.js
@@ -1,5 +1,5 @@
 var is_mobile = !!navigator.userAgent.match(/iphone|android|blackberry/ig) || false;
 
-if(is_mobile){
+if(!is_mobile){
     alert("This website is better viewed on a computer. We recommend to open this on a website.");
 }
