49225d1362a9410757b4f0a0cdeea8a1ee291096
ChobbyCode <jacobhadfield99@gmail.com>
2024-04-17 17:57:41 +0100

    added files

--- assets/scripts/bugReport.js
+++
@@ -1,3 +0,0 @@
-function clickButton(){
-    console.log("Clicked");
-}
--- assets/scripts/checkDeviceType.js
+++
@@ -1,5 +0,0 @@
-// var is_mobile = !!navigator.userAgent.match(/iphone|android|blackberry/ig) || false;
-
-// if(is_mobile){
-//     alert("This website is better viewed on a computer. We recommend to open this on a computer.");
-// }
